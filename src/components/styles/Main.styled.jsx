ec6fd5d5317994e4adf23c72ef9733bd898c9892
laksh <lakshyam38@gmail.com>
2022-04-02 15:29:57 +0530

    main app page basic ui completed

---
+++ src/components/styles/Main.styled.jsx
@@ -0,0 +1,42 @@
+import styled from "styled-components";
+
+export const MainContent = styled.div`
+  height: 100%;
+  width: 100%;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+
+  & > * {
+    margin-top: 3rem;
+  }
+`;
+
+export const Heading = styled.h1`
+  width: 70%;
+  text-align: center;
+  font-size: 2.8rem;
+  font-weight: 600;
+`;
+
+export const Illustration = styled.img`
+  height: 10.5rem;
+  width: auto;
+`;
+
+export const UploadButton = styled.button`
+  background-color: #415f7e;
+  padding: 15px 30px;
+  border: 3px dashed #70d2d7;
+  border-radius: 5px;
+  cursor: pointer;
+  font-size: 1.4rem;
+  font-weight: 600;
+`;
+
+export const Waves = styled.img`
+  width: 100%;
+  position: absolute;
+  bottom: 0%;
+  pointer-events: none;
+`;
--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,5 +1,25 @@
+import { Page } from "../components/styles/Common.styled";
+import {
+  MainContent,
+  Heading,
+  Illustration,
+  UploadButton,
+  Waves,
+} from "../components/styles/Main.styled";
+
 const Main = () => {
-  return <div>app</div>;
+  return (
+    <Page>
+      <MainContent>
+        <Heading>
+          Summarise your recorded online classes in a few minutes.
+        </Heading>
+        <Illustration src="./illustration-upload.png" />
+        <UploadButton>Upload your recorded video lecture</UploadButton>
+        <Waves src="./waves.svg" />
+      </MainContent>
+    </Page>
+  );
 };
 
 export default Main;
