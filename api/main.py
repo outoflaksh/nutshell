64f515bdf77da823da9db2be1fece6a1621803e8
laksh <lakshyam38@gmail.com>
2022-04-02 22:41:52 +0530

    cleanup and reorganisation

--- api/main.py
+++ api/main.py
@@ -1,28 +1,25 @@
 import moviepy.editor as mp
 from deepgram import Deepgram
-import asyncio, json, sys
+import asyncio
 from gensim.summarization.summarizer import summarize
 from pydub import AudioSegment
-import time
 
-start = time.time()
-# extract the audio from the uploaded video
-video = mp.VideoFileClip(r"test-vid.mp4")
-video.audio.write_audiofile(r"extracted.mp3")
+def conv_vid_to_audio(vidpath: str, audiopath: str):
+  # extract the audio from the uploaded video
+  video = mp.VideoFileClip(vidpath)
+  video.audio.write_audiofile(audiopath)
 
-# compress the extrated audio
-# og_audio = AudioSegment.from_mp3("./extracted.mp3")
-# og_audio.export("./result.mp3", format="mp3", bitrate="16k")
+  # compress the extrated audio
+  # og_audio = AudioSegment.from_mp3("./extracted.mp3")
+  # og_audio.export("./result.mp3", format="mp3", bitrate="16k")
 
-# transcribe the obtained audio with deepgram
-DEEPGRAM_API_KEY = 'f6a82cadceb521a5cedf0cefb18629efe4e58097'
+async def transcribe_audio(filepath: str):
+  # transcribe the obtained audio with deepgram
+  DEEPGRAM_API_KEY = 'f6a82cadceb521a5cedf0cefb18629efe4e58097'
 
-FILE = './result.mp3'
+  FILE = filepath
 
-MIMETYPE = 'audio/mp3'
-
-async def main():
-
+  MIMETYPE = 'audio/mp3'
   # Initialize the Deepgram SDK
   deepgram = Deepgram(DEEPGRAM_API_KEY)
   
@@ -57,16 +54,16 @@ async def main():
   except:
     pass
 
-
-
   # Write only the transcript to the console
   return (response["results"]["channels"][0]["alternatives"][0]["transcript"])
 
-transcript = asyncio.run(main())
+def summarise_video(filepath: str):
+  conv_vid_to_audio(filepath, "extracted.mp3")
 
-summary = summarize(transcript, ratio=0.1)
+  transcript = asyncio.run(transcribe_audio("extracted.mp3"))
 
-end = time.time()
+  summary = summarize(transcript, ratio=0.1)
 
-print(summary)
-print(f"took {end-start}s of time")
+  return summary
+
+summarise_video("test-vid.mp4")
