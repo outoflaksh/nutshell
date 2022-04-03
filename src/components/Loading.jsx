373167b0c06c1c0901ac274869e206bdfa44e126
laksh <lakshyam38@gmail.com>
2022-04-03 08:39:38 +0530

    changed loading text

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,4 +1,10 @@
-const Loading = ()=>{
-    return <div>Loading</div>;
+const Loading = () => {
+  return (
+    <div>
+      Processing your video...
+      <br />
+      This could take a few minutes
+    </div>
+  );
 };
 export default Loading;
