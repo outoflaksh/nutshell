9245556e79ef796b5e4e6168567e296df1163b00
laksh <lakshyam38@gmail.com>
2022-04-03 07:57:45 +0530

    font issue fixed

--- src/components/styles/Global.jsx
+++ src/components/styles/Global.jsx
@@ -1,7 +1,7 @@
 import { createGlobalStyle } from "styled-components";
 
 const GlobalStyles = createGlobalStyle`
-    @import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;600;700;800&family=Ubuntu:wght@400;700&display=swap');
+    @import url('https://fonts.googleapis.com/css2?family=Inter:wght@300;400;600;700;800&family=Ubuntu:wght@400;700&display=swap');
 
     * {
         margin: 0;
